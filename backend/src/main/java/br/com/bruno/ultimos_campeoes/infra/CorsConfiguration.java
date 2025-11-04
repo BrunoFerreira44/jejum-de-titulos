1d4cd66895538c664aeb2345a6e88309e18de14d
Bruno Ferreira <brunoferreira1999@live.com>
2025-11-04 18:44:47 -0300

    Adição do docker-compose e dos Dockerfiles

--- backend/src/main/java/br/com/bruno/ultimos_campeoes/infra/CorsConfiguration.java
+++ backend/src/main/java/br/com/bruno/ultimos_campeoes/infra/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.bruno.ultimos_campeoes.infra;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
+    @Value("${FRONTEND_URL:http://127.0.0.1:5500}") // fallback para ambiente local
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Todos os endpoints
-                .allowedOrigins("http://127.0.0.1:5500") // Frontend permitido
+                .allowedOrigins(frontendUrl) // Frontend permitido
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
                 .allowCredentials(true); // Permite cookies/sessões
     }
