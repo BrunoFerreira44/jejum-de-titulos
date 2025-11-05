02627d2fa786c8a7cea87a48ab6ec4132ee72ee5
Bruno Ferreira <brunoferreira1999@live.com>
2025-11-04 22:42:26 -0300

    POI: Aplicação funcional com docker mediante build do backend manualmente

--- backend/src/main/java/br/com/bruno/ultimos_campeoes/infra/CorsConfiguration.java
+++ backend/src/main/java/br/com/bruno/ultimos_campeoes/infra/CorsConfiguration.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
-    @Value("${FRONTEND_URL:http://127.0.0.1:5500}") // fallback para ambiente local
-    private String frontendUrl;
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Todos os endpoints
-                .allowedOrigins(frontendUrl) // Frontend permitido
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://127.0.0.1:5500"
+                ) // Frontends permitidos
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
                 .allowCredentials(true); // Permite cookies/sessões
     }
